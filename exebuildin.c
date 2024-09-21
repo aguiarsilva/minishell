6d989b53d80da6d5549f68365829d32bdd821581
tbui-quo <tbui-quo@student.42wolfsburg.de>
2024-09-21 17:40:40 +0200

    start to work on bulletin struct and echo function

--- exebuildin.c
+++ exebuildin.c
@@ -2,9 +2,13 @@
 #include "lib/minishell.h"
 // add if for each build ins
 
-void run_buildin(char *cmd, char **env)
+int run_buildin(char *cmd, char **env)
 {
-	if (!ft_strncmp(cmd, "echo", ft_strlen(cmd)) && \
+	int	exit_code;
+
+	exit_code = 0;
+	if (ft_strncmp(cmd, "echo", ft_strlen(cmd)) == 0 && \
 			ft_strlen(cmd) == 4)
-	return; // placeholder need to implement echo first
+		exit_code = echo(cmd);
+	return (exit_code); // placeholder need to implement echo first
 }
--- lib/executor.h
+++ lib/executor.h
@@ -7,7 +7,7 @@
 void	executor(char *cmd, char **env);
 
 //ecebuildin.c
-void run_buildin(char *cmd, char **env);
+int		run_buildin(char *cmd, char **env);
 //executorutils.c
 char	*get_env_value_by_name(char *envVarName, char **env);
 char	**parse_command_with_quotes(char *cmd);
