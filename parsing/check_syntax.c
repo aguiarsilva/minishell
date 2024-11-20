065834a3c3a2b979f97af6e4a30a03f93c02ebaa
Bruno Aguiar <aguiarsilva@gmail.com>
2024-11-20 14:38:18 +0100

    changed variable name in syntax checker function

--- parsing/check_syntax.c
+++ parsing/check_syntax.c
@@ -5,25 +5,25 @@
 bool	is_valid_redirection_syntax(t_token *token_list)
 {
 	t_token	*current;
-	int		consecutive_redir_count;
+	int		consecutive_count;
 
 	current = token_list;
-	consecutive_redir_count = 0;
+	consecutive_count = 0;
 	while (current)
 	{
 		if (current->type == REDIR_IN || current->type == REDIR_OUT
 			|| current->type == APPEND || current->type == HEREDOC
 			|| current->type == PIPE)
 		{
-			consecutive_redir_count++;
-			if (consecutive_redir_count > 2)
+			consecutive_count++;
+			if (consecutive_count > 2)
 				return (false);
 			if (!current->next || current->next->type != WORD)
 				return (false);
 		}
 		else
 		{
-			consecutive_redir_count = 0;
+			consecutive_count = 0;
 		}
 		current = current->next;
 	}
