6aaac2bbb0714ecb9e51ae36cdad701ea14a043b
Bruno Aguiar <aguiarsilva@gmail.com>
2024-11-25 13:01:33 +0100

    formatted lexer.h for norm

--- lib/lexer.h
+++ lib/lexer.h
@@ -1,6 +1,6 @@
 
 #ifndef LEXER_H
-#define LEXER_H
+# define LEXER_H
 
 # include "minishell.h"
 
@@ -22,13 +22,14 @@ t_type	get_token_type(char *token);
 void	flush_buffer(t_parser_context *ctx);
 void	treat_quotes(char c, t_buffer_state *state);
 void	process_special_char(char c, t_parser_context *ctx);
-int		is_equals_special(char c, t_char_context *char_ctx, t_buffer_state *state);
+int		is_equals_special(char c, t_char_context *char_ctx,
+			t_buffer_state *state);
 
 //lexer_utils2.c
 int		should_process_double_char(t_char_context *char_ctx);
 void	process_double_char(t_parser_context *ctx, char c, size_t *i);
-void	handle_non_quoted_special_cases(t_parser_context *ctx, t_char_context *char_ctx, char c);
+void	handle_non_quoted_special_cases(t_parser_context *ctx,
+			t_char_context *char_ctx, char c);
 void	handle_special_cases(t_parser_context *ctx, t_char_context *char_ctx);
-
 
 #endif //LEXER_H
